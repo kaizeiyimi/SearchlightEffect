718f6bb9e4bd0698c978b7aaac086216fd7dea1c
kaizeiyimi <kaizeiyimi@126.com>
2016-02-01 18:41:47 +0800

    Create Searchlight.swift

---
+++ Searchlight.swift
@@ -0,0 +1,92 @@
+//
+//  Searchlight.swift
+//  testRadialGradient
+//
+//  Created by kaizei on 16/2/1.
+//  Copyright © 2016年 kaizei. All rights reserved.
+//
+
+import UIKit
+
+public struct SearchlightConfig {
+    var lightColor: UIColor = .whiteColor()
+    var radius:(start: CGFloat, end: CGFloat)? = nil
+    var speed: Double = 120
+}
+
+extension UIView {
+    private static var kSearchlightLayerKey = "kSearchlightLayerKey"
+    
+    func xly_setSearchlight(var config: SearchlightConfig? = nil) {
+        xly_removeSearchlight()
+        maskView = snapshotViewAfterScreenUpdates(true)
+        config = config ?? SearchlightConfig()
+        config?.radius = config?.radius ?? (bounds.height * 0.6, bounds.height * 1.8)
+        
+        let searchlightLayer = SearchlightLayer()
+        searchlightLayer.config = config
+        searchlightLayer.frame = CGRectMake(0, 0, config!.radius!.end * 2, bounds.height)
+        searchlightLayer.position = CGPointMake(bounds.width / 2, bounds.height / 2)
+        
+        let link = CADisplayLink(target: TargetWrapper(searchlightLayer), selector: "linkFired:")
+        searchlightLayer.link = link
+        link.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+        
+        layer.addSublayer(searchlightLayer)
+        searchlightLayer.setNeedsDisplay()
+        
+        objc_setAssociatedObject(self, &UIView.kSearchlightLayerKey, searchlightLayer, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+    
+    func xly_removeSearchlight() {
+        (objc_getAssociatedObject(self, &UIView.kSearchlightLayerKey) as? CALayer)?.removeFromSuperlayer()
+        objc_setAssociatedObject(self, &UIView.kSearchlightLayerKey, nil, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        maskView = nil
+    }
+}
+
+
+final private class SearchlightLayer: CALayer {
+    var config: SearchlightConfig!
+    var link: CADisplayLink!
+    
+    deinit {
+        link?.invalidate()
+    }
+    
+    @objc func linkFired(link: CADisplayLink) {
+        guard let superlayer = superlayer else { return }
+        var nextX = self.position.x + CGFloat(link.duration * config.speed)
+        let endRadius = config.radius!.end
+        if nextX > superlayer.frame.width + endRadius {
+            nextX = -config.radius!.end
+        } else if nextX < -endRadius {
+            nextX = superlayer.frame.width + config.radius!.end
+        }
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        var position = self.position
+        position.x = nextX
+        self.position = position
+        CATransaction.commit()
+    }
+    
+    override func drawInContext(ctx: CGContext) {
+        let locations: [CGFloat] = [0, 1]
+        let gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), [config.lightColor.CGColor, UIColor.clearColor().CGColor], locations)
+        let radius = config.radius!
+        CGContextDrawRadialGradient(ctx, gradient, CGPointMake(bounds.midX, bounds.midY), radius.start, CGPointMake(bounds.midX, bounds.midY), radius.end, .DrawsBeforeStartLocation)
+    }
+}
+
+final private class TargetWrapper: NSObject {
+    weak var target: AnyObject?
+    init(_ target: AnyObject) {
+        self.target = target
+    }
+    
+    private override func forwardingTargetForSelector(aSelector: Selector) -> AnyObject? {
+        return target
+    }
+}
